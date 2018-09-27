f0899e703c37c3767e9dc67b55747981e6141618
OlgaNaranovich <naranovicholga@gmail.com>
2018-09-28 00:03:56 +0300

    zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  const zerosCount = Math.floor(number / 5) + Math.floor(number / 25) + Math.floor(number / 100) - 4;
+  return zerosCount;
 }
