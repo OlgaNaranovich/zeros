fa2801ffa541fc092c7a1127a79123f623547665
OlgaNaranovich <naranovicholga@gmail.com>
2018-09-30 23:02:24 +0300

    zeros

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 module.exports = function getZerosCount(number) {
-  const zerosCount = Math.floor(number / 5) + Math.floor(number / 25) + Math.floor(number / 100) - 4;
+  let zerosCount = 0;
+  while (number > 0) {
+  	number = Math.floor(number / 5);
+  	zerosCount += number;
+  }
   return zerosCount;
 }
